d877761f4c0a486c632e393410f6a2b3967abdee
pusdakom <pusdakom@gmail.com>
2025-11-20 14:01:48 +0000

    oke

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import { renderHtml } from "./renderHtml";
-
 export default {
   async fetch(request, env) {
-    const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
-    const { results } = await stmt.all();
-
-    return new Response(renderHtml(JSON.stringify(results, null, 2)), {
+    const data = {
+      message: 'This is a web service API.',
+      timestamp: new Date().toISOString()
+    };
+    return new Response(JSON.stringify(data, null, 2), {
       headers: {
-        "content-type": "text/html",
+        'content-type': 'application/json;charset=UTF-8',
       },
     });
   },
